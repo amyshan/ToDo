a58ab69e8a283ca513bafa9ed3a2ca7d3323ee73
Amy Shan <amyshan@berkeley.edu>
2016-12-28 23:00:40 -0800

    finished basic todo functionality

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-/**
- * Created by amyshan on 12/28/16.
- */
+function newItem() {
+    var item = document.getElementById('input').value;
+    var ul = document.getElementById("list");
+    var li = document.getElementById('li');
+    li.appendChild(document.createTextNode("- " + item));
+    ul.appendChild(li);
+    document.getElementById('input').value = "";
+    li.onclick = removeItem;
+}
+
+document.body.onkeyup = function(e) {
+    if (e.keyCode == 13) {
+        newItem();
+    }
+}
+
+function removeItem(e) {
+    e.target.parentElement.removeChild(e.target);
+}
